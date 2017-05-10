51a671214418cf9e3df69ec01e6ee971749392d8
0xNacho <nachogarcia91@gmail.com>
2017-05-10 10:54:21 +0200

    Remove unused imports

--- KafkaProducer/src/main/java/com/treelogic/proteus/Runner.java
+++ KafkaProducer/src/main/java/com/treelogic/proteus/Runner.java
@@ -1,23 +1,15 @@
 package com.treelogic.proteus;
 
-import com.treelogic.proteus.hdfs.HDFS;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.stream.Stream;
 
-/**
- * Created by ignacio.g.fernandez on 2/05/17.
- */
+
 public class Runner {
 
     private static final Logger logger = LoggerFactory.getLogger(Runner.class);
-    public static ExecutorService service = Executors.newFixedThreadPool(3); //Max parallelism = stream, hsm and flatness at once.
+    public static ExecutorService service = Executors.newFixedThreadPool(15); //Max parallelism = stream, hsm and flatness at once.
 
     public static void main(String[] args) throws Exception {
         ExecutorService service = Runner.service;
@@ -26,9 +18,8 @@ public class Runner {
         ProteusData.loadData();
         logger.info("Initial data loaded. Starting program.");
 
-        service.submit(new ProteusStreamingTask<>(proteusHDFSFile));
+        service.submit(new ProteusStreamingTask(proteusHDFSFile));
         logger.info("Streaming service task has been submitted");
-
 
         // service.submit();
         //service.shutdown(); //Don't allow more submits
